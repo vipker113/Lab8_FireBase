46f15403715fed2f5bfb8f537a1291e2c6712a5b
vipker113 <79317962+vipker113@users.noreply.github.com>
2022-04-25 17:21:59 +0700

    add screen 3,4

---
+++ app/src/main/java/com/example/lab8_firebase/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.example.lab8_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+}
---
+++ app/src/main/java/com/example/lab8_firebase/RegisterActivity.java
@@ -0,0 +1,4 @@
+package com.example.lab8_firebase;
+
+public class RegisterActivity {
+}
